6f4d5c4542262e040692c571dd7680c914117e99
Rohit kumar <rohit9075@gmail.com>
2020-03-16 23:34:57 +0530

    Button click handled

--- startedservice/src/main/java/com/example/androidservices/MainActivity.java
+++ startedservice/src/main/java/com/example/androidservices/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.androidservices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mStartService, mStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inti method call
+        init();
+    }
+
+    //init method definition
+    private void init() {
+        mStartService = findViewById(R.id.start_service);
+        mStopService = findViewById(R.id.stop_service);
+
+        mStartService.setOnClickListener(this);
+        mStopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_service:
+                Toast.makeText(this, "start service button clicked", Toast.LENGTH_SHORT).show();
+
+                case R.id.stop_service:
+                Toast.makeText(this, "stop service button clicked", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
